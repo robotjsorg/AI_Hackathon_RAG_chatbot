d637198cd5503c3b21f9fd7c1cdd5ce0a6a7d746
Joseph McMahon <jmcmahon443@gmail.com>
2024-10-26 12:38:17 -0700

    added hello message response

--- app.py
+++ app.py
@@ -14,6 +14,11 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 # Register Listeners
 register_listeners(app)
 
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
 # Start Bolt app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
