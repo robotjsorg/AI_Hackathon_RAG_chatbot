56a8f807ae1a8fccfa7164cd7507540b8d02102d
Irreel <phantasy0@qq.com>
2024-10-26 14:08:37 -0700

    Add dataloader for VectorDB

--- server/app.py
+++ server/app.py
@@ -2,22 +2,41 @@ import os
 from flask import Flask, request, jsonify
 
 from utils import init_vectordb, add_documents_to_vectordb
-from langchain_openai import OpenAIEmbeddings
+import chromadb.utils.embedding_functions as embedding_functions
 
 from dotenv import load_dotenv
 
 load_dotenv()  # Load environment variables from .env file
-openai_api_key = os.getenv("OPENAI_API_KEY")
-embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+embed_fn = embedding_functions.OpenAIEmbeddingFunction(
+                api_key=os.getenv("OPENAI_API_KEY"),
+                model_name="text-embedding-3-small"
+            )
+
 
 app = Flask(__name__)
 
+global doc_cnt
 # Initialize the RAG database when the server starts
-rag_db = init_vectordb(embeddings)
+rag_db, doc_cnt = init_vectordb(embed_fn)
 
 @app.route('/')
 def home():
     return "RAG-enabled Flask Server is running!"
+
+@app.route('/add_documents', methods=['POST'])
+def add_documents():
+    global doc_cnt
+    data = request.json
+    
+    add_documents_to_vectordb(data, rag_db, doc_cnt=doc_cnt)
+    
+    return "Documents added to vector database"
+
+@app.route('/retrieve', methods=['POST'])
+def retrieve_documents():
+    data = request.json
+    context = retrieve_documents_from_vectordb(data, rag_db)
+    return context
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/utils.py
+++ server/utils.py
@@ -1,24 +1,84 @@
+import os
+from pathlib import Path
+
 import chromadb
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import BSHTMLLoader, TextLoader
 
+from config import DATA_DIR
+
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size=500,
+    chunk_overlap=20,
+    length_function=len,
+    is_separator_regex=False,
+)
 
 # Initialize the RAG database
-def init_vectordb(embeddings_function):
-    persistent_client = chromadb.PersistentClient()
+def init_vectordb(embeddings_fn, path="./chroma/"):
+    chroma_client = chromadb.PersistentClient(path=path)
+    chroma_client.reset() 
 
-    db = Chroma(
-        client=persistent_client,
-        collection_name="it_support",
-        embedding_function=embeddings_function,
+    db = chroma_client.create_collection(
+        name="it_support",
+        embedding_function=embeddings_fn,
+    )     
+    
+    doc_cnt = load_static_data(db)
+    
+    return db, doc_cnt
+
+
+def load_static_data(db):
+    data = []
+
+    # Load html files from the data directory
+    html_files = Path(DATA_DIR).glob('*.html')
+    for html_file in html_files:    
+        print(f"Loading {html_file}")
+        loader = BSHTMLLoader(str(html_file))
+        data.extend(loader.load())
+    
+    text_files = Path(DATA_DIR).glob('*.txt')
+    for text_file in text_files:
+        print(f"Loading {text_file}")
+        loader = TextLoader(str(text_file))
+        data.extend(loader.load())
+        
+    # Split documents and add to the database
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    data_chunks = text_splitter.split_documents(data)
+
+    doc_ids = ["init_"+str(i) for i in range(len(data_chunks))]
+    
+    db.add(
+        documents=[chunk.page_content for chunk in data_chunks], 
+        # metadata=[dict(chunk).get("metadata", {}) for chunk in data_chunks],
+        ids=doc_ids)
+    
+    return len(doc_ids)
+
+
+def add_documents_to_vectordb(documents, db, metadata=None, doc_cnt=0):
+    # text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    # split_docs = text_splitter.split_documents(documents)
+    # db.add_documents(
+        # split_docs)
+    db.add(
+        documents=documents,
+        metadata=metadata,
+        ids=["id1", "id2", "id3", ...]
     )
-     
-    return db
 
 
-def add_documents_to_vectordb(documents, db: Chroma):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-    split_docs = text_splitter.split_documents(documents)
-    db.add_documents(split_docs)
-    db.persist()
+def query_documents_from_vectordb(query, db, k=3):
+    docs = db.query(
+        query_texts=[query],
+        n_results=k,
+        # where={"metadata_field": "is_equal_to_this"},
+        # where_document={"$contains":"search_string"}
+    )
 
+    return docs
